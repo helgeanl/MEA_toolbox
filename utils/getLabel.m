f075b3cef26b44a9eda054d3545d7753fca3ea96
Helge-Andre Langåker <helgeanl@ntnu.no>
2017-11-21 16:04:28 +0100

    Add ability for using vectors as input and output

--- utils/getLabel.m
+++ utils/getLabel.m
@@ -1,9 +1,12 @@
-function label  = getLabel( index )
+function label  = getLabel( index,labels )
 %getLabel Returns the channel label of given index
 %   Channel index is ranging from 1 to 60 (the indexes in the channel array), 
 %   while the labels corresponds to the MEA layout 60MEA200/30iR.  
 %   Labels 12-17 in he first column, 21-28 in the second, ..., 71-78 in the 
 %   seventh, and 82-87 in the last.
-    labels = getLabels();
-    label = labels{index};
+   % labels = getLabels();
+   
+    for i=1:length(index)
+        label{i} = labels{index(i)};
+    end
 end
--- utils/getLabelIndex.m
+++ utils/getLabelIndex.m
@@ -1,10 +1,48 @@
-function index  = getLabelIndex( label )
+function index  = getLabelIndex( label,labels )
 %getLabelIndex Returns the channel index of given label
+%   index  = getLabelIndex( label,labels ) returns the array index of
+%   given label. Label may be a scalar, numeric vector, or a cell array
+%   containing label strings. Labels is an n-element cell array containing all the label
+%   strings.
+%
 %   Channel index is ranging from 1 to 60 (the indexes in the channel array), 
 %   while the labels corresponds to the MEA layout 60MEA200/30iR.  
 %   Labels 12-17 in he first column, 21-28 in the second, ..., 71-78 in the 
 %   seventh, and 82-87 in the last.
-    labels = getLabels();
-    index = find(contains(labels,num2str(label)));
+
+    
+    
+
+    if isnumeric(label)
+        index = zeros(length(label),1);
+        for i=1:length(label)
+            if label(i)==15
+                index(i) = find(contains(labels,'Ref'));
+            else
+                index(i) = find(contains(labels,num2str(label(i))));
+            end
+        end
+    
+    else
+        index = zeros(length(label),1);
+        for i=1:length(label)
+            if strcmp(label(i),'15')|| strcmp(label(i),'ref')
+                index(i) = find(contains(labels,'Ref'));
+            else
+                find(contains(labels,label(i)));
+                index(i) = find(contains(labels,label(i)));
+            end
+        end
+%         index = zeros(size(label,1),1);
+%         for i=1:size(label,1)
+%             if strcmp(label(i,:),'15')|| strcmp(label(i,:),'ref')
+%                 index(i) = find(contains(labels,'Ref'));
+%             else
+%                 index(i) = find(contains(labels,label(i,:)));
+%             end
+%         end
+    end
+    
+    
 end
 
