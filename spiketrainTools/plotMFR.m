ea2cdc2147d786ed00d1d7c1d1f2ddac68ff6942
Helge-Andre Langåker <helgeanl@ntnu.no>
2017-12-13 16:00:46 +0100

    Add extra figure of heatmap with MFR in clear text

--- spiketrainTools/plotMFR.m
+++ spiketrainTools/plotMFR.m
@@ -4,6 +4,30 @@ function plotMFR( timeStamps,labels,duration )
 %   containing the spike data for each channel, the duration [seconds] of 
 %   the selected recordring, and generates a heatmap of the mean firing rate
 %   for each channel.
+    
+    label = 11;
+    heat = zeros(8,8);
+    for j = 1:8
+        for i = 1:8
+            if ~((i==1 || i==8)&& (j==1 || j==8))
+                if label == 15 % Channel 15 is called 'Ref' in the data
+                    index =find(contains(labels,'Ref','IgnoreCase',true));
+                else
+                    index =find(contains(labels,num2str(label),'IgnoreCase',true));
+                end
+                heat(i,j) = numel(timeStamps{index})/duration;
+            end
+            if mod(label,10) < 8
+                label = label +1;
+            else
+            label = label +3;
+            end
+        end
+    end
+    figure;
+    heatmap(heat);
+    title('Mean firing rate [spike/s]')
+
 
     % Replace channel label 'Ref' with '15'
     refIndex = find(contains(labels,'Ref'));
