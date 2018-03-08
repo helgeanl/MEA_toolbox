16915c351ea2ba28969707b959c909a48a04fdcc
Helge-Andre Langåker <helgeanl@ntnu.no>
2018-03-08 20:32:29 +0100

    Bugfix: Rasterplot sorting
    Bug happened due to the inability to save changes in a Mcs object,
    which is troubling when we want to sort the arrays to make up for
    different channel orders.
    
    The solution is a hack where we copy the entire rasterplot code from
    the Msc package and replace the timestamp object with a normal
    cell array. Hopefully this will work...

--- spiketrainTools/rasterplot.m
+++ spiketrainTools/rasterplot.m
@@ -3,6 +3,11 @@ function rasterplot( timeStampData )
 %   Plot spike time stamps as a rasterplot
 %   Takes in a time stamp data object (McsTimeStampStream) 
 %   and generates a heatmap with number of spikes per channel
+%
+%   The code to map the timestamps to rasterplot is copied from 
+%   the McsTimeStampStream plot function in the McsMatlabDataTools package.
+%   (It is possible to use plot(timeStampData, []) to get the same result,
+%    but due to trouble with inconsistent channel order we use this hack.)
 
     timeStamps = timeStampData.TimeStamps;
     labels = timeStampData.Info.Label;
@@ -13,11 +18,37 @@ function rasterplot( timeStampData )
         labels{refIndex} = '15'; 
     end
     [timeStampData.Info.Label, s] = sort(labels);
-    timeStampData.TimeStamps = timeStamps(s);
+    timeStamps = timeStamps(s);
  
     figure;
-    plot(timeStampData,[]); 
+    % --- Code from McsTimeStampStream plot start ---
+    lineLength = 0.3;
+    M = cell(length(timeStamps),2);
+    emptyTimeStamps = false(1,length(timeStamps));
+    for timei = 1:length(timeStamps)
+        if isempty(timeStamps{timei})
+            emptyTimeStamps(timei) = true;
+            continue
+        end
+        M{timei,1} = McsHDF5.TickToSec([timeStamps{timei} ; timeStamps{timei}]);
+        M{timei,2} = repmat([timei-lineLength ; timei+lineLength],1,size(M{timei,1},2));
+    end
+    if all(emptyTimeStamps)
+        return
+    end
+    for timei = 1:length(timeStamps)
+        if emptyTimeStamps(timei)
+            continue
+        end
+        line(M{timei,1},M{timei,2},'Color','k')
+    end
+    set(gca,'YTick',1:length(timeStamps));
+    set(gca,'YTickLabel',strtrim(labels));
+    ylabel('Source Channel')
+    % --- Code from McsTimeStampStream plot end ---
+    
+    xlabel('Time [s]')
     title('Rasterplot');
-    ylim([0 length(timeStampData.TimeStamps)+1]);
+    ylim([0 length(labels)+1]);
 end
 
